b103b4468c05779f134bb48eb0f17e2953c4a131
Berivan Kuerekci <berivan.kuerekci@students.unibe.ch>
2018-11-21 17:28:10 +0100

    some stuff

--- frontend/pages/jobs.tsx
+++ frontend/pages/jobs.tsx
@@ -68,7 +68,6 @@ export const JobPage: React.SFC<JobPageProps> = ({ loading, error, data }) => {
           <Link href={"/jobs/create"} passHref>
             <Button icon={"plus"} as={"a"} />
           </Link>
-
           <Item.Group divided>
             {data.jobs.map(job => (
               <JobItem key={job.id} job={job} />
