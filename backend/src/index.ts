723b85ad73cc95e6297cad50e284a43583a087a3
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-23 12:51:58 +0100

    Only seed admin user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,56 +42,8 @@ createConnection({
       .length > 0;
 
   if (!adminAvailable) {
-    // SEED TODO env-variable for seeding
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from(Job)
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from(Organization)
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from(User)
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from("bookmarks")
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from(JobApplication)
-      .execute();
-
-    const exampleOrgs = ["Organization 1", "Organization 2", "Organization 3"];
-    const orgs = [];
-
-    for (let i = 0; i < exampleOrgs.length; i++) {
-      const org = await organizationRepository.createOrganization(
-        exampleOrgs[i]
-      );
-      orgs.push(org);
-    }
-
-    const exampleJobs = ["Job 1", "Job 2", "Job 3"];
-    for (let i = 0; i < exampleJobs.length; i++) {
-      await jobRepository.createJob({
-        input: {
-          title: exampleJobs[i],
-          organization: orgs[i].id,
-          description: "This is Job " + (i + 1),
-          start: new Date(),
-          salary: 10
-        }
-      });
-    }
-
+    // Only seed admin user
+    // TODO this has to be in a database patch later
     const admin = new User();
     admin.email = "admin@example.com";
     admin.firstname = "Noe";
@@ -99,51 +51,6 @@ createConnection({
     admin.password = bcrypt.hashSync("123456", bcrypt.genSaltSync(10));
     admin.phone = "+41 123 456 34 34";
     admin.siteAdmin = true;
-
-    await connection.getRepository(User).save(admin);
-
-    const employee = new User();
-    employee.email = "employee@example.com";
-    employee.firstname = "Miles";
-    employee.lastname = "Stone";
-    employee.password = bcrypt.hashSync("123456", bcrypt.genSaltSync(10));
-    employee.phone = "+41 987 654 76 76";
-
-    await connection.getRepository(User).save(employee);
-
-    const student = new User();
-    student.email = "student@example.com";
-    student.firstname = "Kim";
-    student.lastname = "Possible";
-    student.password = bcrypt.hashSync("123456", bcrypt.genSaltSync(10));
-    student.phone = "+41 195 321 99 99";
-
-    await connection.getRepository(User).save(student);
-
-    await connection
-      .createQueryBuilder()
-      .relation(User, "employer")
-      .of(employee.id)
-      .add(orgs[1]);
-
-    const exampleRoles = ["Role 1", "Role 2", "Role 3"];
-    const roles = [];
-
-    for (let i = 0; i < exampleOrgs.length; i++) {
-      const role = await roleRepository.createRole({
-        title: exampleRoles[i],
-        description: "This is " + exampleRoles[i]
-      });
-      roles.push(role);
-    }
-
-    for (let i = 0; i < roles.length; i++) {
-      await connection
-        .createQueryBuilder()
-        .relation(User, "roles")
-        .of(student.id)
-        .add(roles[i]);
-    }
   }
 
   // @ts-ignore
