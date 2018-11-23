bf9de4c2ede3472d7107fe3878074343e039b5cc
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-23 13:07:10 +0100

    Add more users via demeter

--- backend/dev/demeter/users/admin.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  email: "admin@example.com",
-  firstname: "Noe",
-  lastname: "Müller",
-  phone: "+41 123 456 34 34",
-  siteAdmin: true
-};
--- backend/dev/demeter/users/index.ts
+++ backend/dev/demeter/users/index.ts
@@ -1,3 +1,7 @@
-import admin from "./admin";
+import yannik from "./yannik";
+import berivan from "./berivan";
+import fabio from "./fabio";
+import luca from "./luca";
+import dilara from "./dilara";
 
-export default [admin]
+export default [berivan, fabio, luca, dilara, yannik];
