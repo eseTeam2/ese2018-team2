f0f28de710bd1a0947d2db77a56e548db1ab18e8
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-25 19:50:01 +0100

    Create me page component and ensure logged in

--- frontend/pages/me.tsx
+++ frontend/pages/me.tsx
@@ -1,17 +1,5 @@
-import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import NavBar from "../components/Frame/NavBar";
 import { withIntialMe } from "../lib/withMe";
-import { Container, Header } from "semantic-ui-react";
-
-const Page = () => (
-  <React.Fragment>
-    <NavBar />
-    <Container>
-      <Header as={"h2"}>Profil</Header>
-      <p>TODO</p>
-    </Container>
-  </React.Fragment>
-);
+import Page from "../components/Me/Page";
 
 export default withIntialMe(Page);
