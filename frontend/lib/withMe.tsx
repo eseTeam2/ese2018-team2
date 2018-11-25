0368599ee7cea2d6f0a2c9efb7e7652cf73688a2
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-25 15:19:08 +0100

    Add documentation for HOCs

---
+++ frontend/lib/withMe.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import gql from "graphql-tag";
+import { NextContext } from "next";
+import { graphql, ApolloConsumer } from "react-apollo";
+
+export interface WithMeProps {
+  me?: { id: string; firstname: string; lastname: string };
+}
+
+const query = gql`
+  query WithMe {
+    me {
+      id
+      firstname
+      lastname
+    }
+  }
+`;
+
+/**
+ * Useful HOC to get current user as initial props.
+ * Only works with pages, as getInitialProps is only executed on the first component of the tree.
+ */
+export const withIntialMe = <P extends object>(
+  Component: React.ComponentType<P>
+) =>
+  class extends React.Component<P & WithMeProps> {
+    static async getInitialProps(ctx: NextContext) {
+      //@ts-ignore
+      const client = ctx.apolloClient;
+
+      const defaultReturn = { data: { me: null } };
+      const {
+        data: { me }
+      } = client.query({ query }).catch(() => defaultReturn);
+
+      return {
+        me
+      };
+    }
+
+    render() {
+      return <Component {...this.props} />;
+    }
+  };
+
+/**
+ * Useful HOC to get current user.
+ */
+export const withMe = graphql<WithMeProps>(query, {
+  //@ts-ignore
+  props: ({ data }) => ({
+    //@ts-ignore
+    me: data.me
+  })
+});
