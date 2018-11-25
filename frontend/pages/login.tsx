c52b49176a92ebe7cb1f76c12843fc7ff9912cdd
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-25 19:51:08 +0100

    Clean up login page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Grid, Image } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import Login from "../components/Auth/Login";
+import NavBar from "../components/Frame/NavBar";
 
 export default () => (
   <React.Fragment>
-    <Grid columns={2}>
-      <Grid.Column>
-        <Grid columns={2}>
-          <Grid.Column>
-            <Image centered src={"../static/logo_04.png"} />
-          </Grid.Column>
-        </Grid>
-      </Grid.Column>
-    </Grid>
-    <Login />
+    <NavBar />
+    <Container>
+      <Login />
+    </Container>
   </React.Fragment>
 );
