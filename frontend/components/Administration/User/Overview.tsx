fce3fea6886468a49bbdbc04fd235303868873d2
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-27 13:21:37 +0100

    Add details for users

--- frontend/components/Administration/User/Overview.tsx
+++ frontend/components/Administration/User/Overview.tsx
@@ -3,6 +3,7 @@ import Router from "next/router";
 import React from "react";
 import { Query } from "react-apollo";
 import { Breadcrumb, Icon, Radio, Segment, Table } from "semantic-ui-react";
+import IsDetail from "./IsDetail";
 
 const query = gql`
   query GET_USERS($onlyAdmins: Boolean) {
@@ -32,70 +33,76 @@ class Overview extends React.Component<OverviewProps, OverviewState> {
 
   render() {
     return (
-      <React.Fragment>
-        <Breadcrumb size="big">
-          <Breadcrumb.Section>Übersicht</Breadcrumb.Section>
-        </Breadcrumb>
-        <Segment basic>
-          <Radio
-            toggle
-            checked={this.state.onlyAdmins}
-            label={"Nur Admin"}
-            onChange={this.toggleOnlyAdmins}
-          />
-        </Segment>
-        <Query query={query} variables={{ onlyAdmins: this.state.onlyAdmins }}>
-          {({ loading, error, data }) => (
-            <Segment basic loading={loading}>
-              {error && <p>{error.message}</p>}
-              {!error && (
-                <Table singleLine fixed selectable>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell>Email</Table.HeaderCell>
-                      <Table.HeaderCell>Vornamen</Table.HeaderCell>
-                      <Table.HeaderCell>Nachnamen</Table.HeaderCell>
-                      <Table.HeaderCell>Admin</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {data.users &&
-                      data.users.map(user => (
-                        <Table.Row
-                          key={user.id}
-                          onClick={async e => {
-                            e.preventDefault();
-                            await Router.push({
-                              pathname: "/admin/users",
-                              query: {
-                                detail: user.id
-                              }
-                            });
-                          }}
-                        >
-                          <Table.Cell>{user.email}</Table.Cell>
-                          <Table.Cell>{user.firstname}</Table.Cell>
-                          <Table.Cell>{user.lastname}</Table.Cell>
-                          <Table.Cell>
-                            {user.siteAdmin ? (
-                              <Icon
-                                color="green"
-                                name="checkmark"
-                                size="large"
-                              />
-                            ) : (
-                              ""
-                            )}
-                          </Table.Cell>
-                        </Table.Row>
-                      ))}
-                  </Table.Body>
-                </Table>
-              )}
-            </Segment>
-          )}
-        </Query>
-      </React.Fragment>
+      <IsDetail>
+        <React.Fragment>
+          <Breadcrumb size="big">
+            <Breadcrumb.Section>Übersicht</Breadcrumb.Section>
+          </Breadcrumb>
+          <Segment basic>
+            <Radio
+              toggle
+              checked={this.state.onlyAdmins}
+              label={"Nur Admin"}
+              onChange={this.toggleOnlyAdmins}
+            />
+          </Segment>
+
+          <Query
+            query={query}
+            variables={{ onlyAdmins: this.state.onlyAdmins }}
+          >
+            {({ loading, error, data }) => (
+              <Segment basic loading={loading}>
+                {error && <p>{error.message}</p>}
+                {!error && (
+                  <Table singleLine fixed selectable>
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.HeaderCell>Email</Table.HeaderCell>
+                        <Table.HeaderCell>Vornamen</Table.HeaderCell>
+                        <Table.HeaderCell>Nachnamen</Table.HeaderCell>
+                        <Table.HeaderCell>Admin</Table.HeaderCell>
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      {data.users &&
+                        data.users.map(user => (
+                          <Table.Row
+                            key={user.id}
+                            onClick={async e => {
+                              e.preventDefault();
+                              await Router.push({
+                                pathname: "/admin/users",
+                                query: {
+                                  detail: user.id
+                                }
+                              });
+                            }}
+                          >
+                            <Table.Cell>{user.email}</Table.Cell>
+                            <Table.Cell>{user.firstname}</Table.Cell>
+                            <Table.Cell>{user.lastname}</Table.Cell>
+                            <Table.Cell>
+                              {user.siteAdmin ? (
+                                <Icon
+                                  color="green"
+                                  name="checkmark"
+                                  size="large"
+                                />
+                              ) : (
+                                ""
+                              )}
+                            </Table.Cell>
+                          </Table.Row>
+                        ))}
+                    </Table.Body>
+                  </Table>
+                )}
+              </Segment>
+            )}
+          </Query>
+        </React.Fragment>
+      </IsDetail>
     );
   }
 }
