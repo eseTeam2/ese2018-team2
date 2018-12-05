485e342827da842708367265304b028c7b4f5437
Fabio Bertagna <flbertagna@gmail.com>
2018-12-05 13:53:34 +0100

    Added Styling buttons

--- frontend/components/Slate/SlateEditor.tsx
+++ frontend/components/Slate/SlateEditor.tsx
@@ -1,14 +1,15 @@
 import { Value } from "slate";
-import { Container, Segment } from "semantic-ui-react";
+import { Container, Segment, Button, Icon } from "semantic-ui-react";
 import { Editor } from "slate-react";
 import { initialValue } from "./initialValue";
 import * as React from "react";
-import { Simulate } from "react-dom/test-utils";
+import { type } from "os";
 
 interface SlateEditorProps {}
 
 interface SlateEditorState {
   value: Value;
+  editor?: Editor;
 }
 
 class SlateEditor extends React.Component<SlateEditorProps, SlateEditorState> {
@@ -43,10 +44,18 @@ class SlateEditor extends React.Component<SlateEditorProps, SlateEditorState> {
         change.toggleMark("underlined");
         return true;
       }
+      case "<": {
+        change.toggleMark("code");
+        return true;
+      }
       default: {
         return;
       }
     }
+  };
+
+  ref = editor => {
+    this.setState({ editor: editor });
   };
 
   renderMark = (props, editor, next) => {
@@ -66,13 +75,44 @@ class SlateEditor extends React.Component<SlateEditorProps, SlateEditorState> {
     }
   };
 
+  onClickMark = (e, type) => {
+    e.preventDefault();
+    this.state.editor.toggleMark(type);
+  };
+
+  hasMark = type => {
+    const value = this.state.value;
+    return value.activeMarks.some(mark => mark.type == type);
+  };
+
+  renderMarkButton = (type, icon) => {
+    const isActive = this.hasMark(type);
+    return (
+      <Button
+        icon
+        active={isActive}
+        onMouseDown={event => this.onClickMark(event, type)}
+      >
+        <Icon name={icon} />
+      </Button>
+    );
+  };
+
   render() {
     return (
       <Container>
         <Segment.Group>
-          <Segment />
+          <Segment>
+            <Button.Group>
+              {this.renderMarkButton("bold", "bold")}
+              {this.renderMarkButton("italic", "italic")}
+              {this.renderMarkButton("underlined", "underline")}
+              {this.renderMarkButton("code", "code")}
+            </Button.Group>
+          </Segment>
           <Segment>
             <Editor
+              ref={this.ref}
               value={this.state.value}
               onChange={this.onChange}
               onKeyDown={this.onKeyDown}
