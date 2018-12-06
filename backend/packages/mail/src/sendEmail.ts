2c065214a9330e8d04d901e0d7b06e5976a05f06
Fabio Bertagna <flbertagna@gmail.com>
2018-12-06 17:41:09 +0100

    Fixed building error

--- backend/packages/mail/src/sendEmail.ts
+++ backend/packages/mail/src/sendEmail.ts
@@ -1,19 +1,17 @@
-import { checkAPIKey, client } from "../index";
 import SparkPost from "sparkpost";
 import config from "@unijobs/backend-modules-config";
 
 export const sendEmail = async (recipient: string) => {
   try {
     const client = new SparkPost(config.get("sparkpost_api_key"));
-    if (checkAPIKey) {
-      const response = await client.transmissions.send({
-        options: {
-          sandbox: true
-        },
-        content: {
-          from: "testing@sparkpostbox.com",
-          subject: "Confirm Email",
-          html: `
+    const response = await client.transmissions.send({
+      options: {
+        sandbox: true
+      },
+      content: {
+        from: "testing@sparkpostbox.com",
+        subject: "Confirm Email",
+        html: `
             <html>
                 <body>
                     <p>Testing SparkPost - the world's most awesome email service!</p>
@@ -21,11 +19,10 @@ export const sendEmail = async (recipient: string) => {
                 </body>
             </html>
             `
-        },
-        recipients: [{ address: recipient }]
-      });
-      console.log(response);
-    }
+      },
+      recipients: [{ address: recipient }]
+    });
+    console.log(response);
     return true;
   } catch (e) {
     console.log(
