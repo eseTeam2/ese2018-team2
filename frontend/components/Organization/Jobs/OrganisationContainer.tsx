58e685824cbeb26a233aa9a658b5180a38338e8a
Fabio Bertagna <flbertagna@gmail.com>
2018-12-11 00:34:03 +0100

    Changed Queries

--- frontend/components/Organization/Jobs/OrganisationContainer.tsx
+++ frontend/components/Organization/Jobs/OrganisationContainer.tsx
@@ -9,29 +9,32 @@ import {
 import * as React from "react";
 import Link from "next/link";
 import OrganizationOverviewItem from "./OrganizationOverviewItem";
+import { ApolloError } from "apollo-client";
 
 interface Job {
   id: string;
   title: string;
   description: string;
-  salary: number;
+  applicationCount: number;
 }
 
-interface Organisation {
+interface Organization {
   id: string;
   name: string;
-  jobs: {
-    id: string;
-    title: string;
-  };
 }
 
 interface OrganisationContainerProps {
-  org: Organisation;
+  org: Organization;
+  loading: boolean;
+  error: ApolloError;
+  jobs: Job[];
 }
 
 const OrganisationContainer: React.FC<OrganisationContainerProps> = ({
-  org
+  org,
+  loading,
+  error,
+  jobs
 }) => (
   <Container>
     <Header as="h3" attached="top" block>
@@ -73,7 +76,7 @@ const OrganisationContainer: React.FC<OrganisationContainerProps> = ({
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {org.jobs.map(job => (
+          {jobs.map(job => (
             <OrganizationOverviewItem key={job.id} job={job} />
           ))}
         </Table.Body>
